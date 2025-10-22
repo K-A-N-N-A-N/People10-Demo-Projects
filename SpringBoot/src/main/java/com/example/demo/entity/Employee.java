ba9035982a645a3f45b6568426af233a1ab38118
Kannan.M.D <65282953+K-A-N-N-A-N@users.noreply.github.com>
2025-10-22 22:57:21 +0530

    Connected to Mysql Db

--- SpringBoot/src/main/java/com/example/demo/entity/Employee.java
+++ SpringBoot/src/main/java/com/example/demo/entity/Employee.java
@@ -1,11 +1,9 @@
 package com.example.demo.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "Employee")
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
